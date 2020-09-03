1c3ec6a9454b69ce2a1e2f6f47fe218467c62a4a
John <K2023769@gmail.com>
2020-09-03 16:32:33 +0400

    buid app files

---
+++ precache-manifest.324240f7135854c34691a01042aec032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e950ce6c83800b857e236dbf76dff093",
+    "url": "/clientsportal/index.html"
+  },
+  {
+    "revision": "89783ffb616a08a44f1c",
+    "url": "/clientsportal/static/css/2.bc3f430c.chunk.css"
+  },
+  {
+    "revision": "9eb28eb91907203b65b5",
+    "url": "/clientsportal/static/css/main.e0f56a58.chunk.css"
+  },
+  {
+    "revision": "89783ffb616a08a44f1c",
+    "url": "/clientsportal/static/js/2.f959e2c9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/clientsportal/static/js/2.f959e2c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eb28eb91907203b65b5",
+    "url": "/clientsportal/static/js/main.5966b1bb.chunk.js"
+  },
+  {
+    "revision": "fee6fe9268bee7c16537",
+    "url": "/clientsportal/static/js/runtime-main.a19b52ac.js"
+  }
+]);
